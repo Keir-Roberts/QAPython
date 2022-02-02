f77e77621a1527c2d5031afe96113b05f88b61dd
Keir <keirroberts99@gmail.com>
2022-02-02 16:08:03 +0000

    edited ends-with-py

--- ends-with-py.py
+++ ends-with-py.py
@@ -1,5 +1,8 @@
 # Create a code that returns a boolean of True if a given string ends with 'py'
 # and false otherwise.
 
-phrase = input("Input a string ")
-print(phrase.endswith("py"))
+def pyend(phrase):
+    return (phrase.lower()).endswith("py")
+
+
+print(pyend(input("Write a string")))
