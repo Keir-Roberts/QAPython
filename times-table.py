8f96c63ad32444e0b179384a91b615db63870bdc
Keir <keirroberts99@gmail.com>
2022-02-03 09:54:27 +0000

    Made timestable into a function

--- times-table.py
+++ times-table.py
@@ -1,21 +1,34 @@
 # A piece of code to generate n amounts of values in x times tables.
 
-#Initial error code and taking inputs
+# Initial error code and taking inputs
 
 try:
     while True:
-        n = int(input("How many values in each table? "))
-        x = int(input("How many times tables do you want? "))
+        num = int(input("How many values in each table? "))
+        amount = int(input("How many times tables do you want? "))
         break
 except ValueError:
     print("Only input integers please! ")
 
-# A nested if statement that creates a new list, 
-# fills it with the multiplication of the current number between 1 and x, with the numbers between 1 and n,
-# then prints off that list as a string seperated by spaces, before repeating with a 1 higher digit.
+    
+# A function that creates a list to store each times table
+# A nested if statement that creates a new list,
+# fills the list with the multiplication of current number between 1 and x,
+# with numbers between 1 and n, then adds a string of that list into biglist,
+# seperated by spaces, before repeating with a 1 higher digit.
+# When all timestables are made, biglist is coverted into a string,
+# seperated by new lines which is returned as output
 
-for digit in range(1, x+1):
-    list = []
-    for number in range(1, n+1):
-        list.append(str(digit * number))
-    print(" ".join(list))
+
+def timestable(n, x):
+    biglist = []
+    for digit in range(1, x+1):
+        list = []
+        for number in range(1, n+1):
+            list.append(str(digit * number))
+        biglist.append("\t".join(list))
+    output = "\n".join(biglist)
+    return output
+
+
+print(timestable(num, amount))
